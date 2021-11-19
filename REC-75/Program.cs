1ea2726846652aa5462536ee6a5877e90f555a21
сергей токарев <sseregatokarew@gmail.com>
2021-11-19 23:12:35 +0700

    Добавьте файлы проекта.

---
+++ REC-75/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace REC_76
+{
+    class Program
+    {
+        static int SumRecursion(int[] arr , int i=0)
+        {
+            if (i >= arr.Length)
+                return 0;
+            int sum = SumRecursion(arr, i+1);
+            return arr[i] + sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа через запятую: ");
+            var ARR = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            Console.Write(SumRecursion(ARR));
+            Console.ReadKey();
+        }
+    }
+}
